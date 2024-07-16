b149b855a30089dd1bd20d50ee40738fcc5f9bb0
Scott Hanson <computergeek1507@users.noreply.github.com>
2024-07-15 23:46:53 -0400

    Update fpp_install.sh

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-# fpp-plugin-tplink install script
+!/bin/bash
 
 BASEDIR=$(dirname $0)
 cd $BASEDIR
@@ -9,7 +7,9 @@ cd ..
 sudo apt install python3.11-venv
 
 python3 -m venv --system-site-packages env
-# sudo apt-get -y install pip
+
 source env/bin/activate
+
 sudo env/bin/pip install python-kasa
+
 make
