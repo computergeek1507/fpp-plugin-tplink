5e3a47d4fd9dfd78125e4e54841e14c26374e9a9
Scott Hanson <computergeek1507@users.noreply.github.com>
2026-07-25 11:40:23 -0400

    Remove render_GET method from TPLinkPlugin
    Removed the render_GET method from the TPLinkPlugin class.

--- src/tplinkPlugin.cpp
+++ src/tplinkPlugin.cpp
@@ -398,11 +398,6 @@ public:
         CommandManager::INSTANCE.addCommand(new TPLinkAllSwitchesToggleCommand(this));
     }
 
-    virtual HTTP_RESPONSE_CONST std::shared_ptr<httpserver::http_response> render_GET(const httpserver::http_request &req) override {
-        std::string v = getTopics();
-        return std::shared_ptr<httpserver::http_response>(new httpserver::string_response(v, 200));
-    }
-    
     virtual void modifySequenceData(int ms, uint8_t *seqData) override {
         try
         {
